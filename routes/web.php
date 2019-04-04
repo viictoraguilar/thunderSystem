80d857d609348980c95aa8dba7069e8cac3a1111
Victor Aguilar <victorhugo.webdeveloper@gmail.com>
2019-04-04 12:22:08 -0600

    Añadiendo archivos del repositorio completo

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'HomeController@index');
+
+Route::get('/dashboard', 'DashboardController@index');
+
+Route::resource('/expense_reports', 'ExpenseReportController');
+Route::get('/expense_reports/{id}/confirmDelete', 'ExpenseReportController@confirmDelete');
+Route::get('/expense_reports/{id}/confirmSendMail', 'ExpenseReportController@confirmSendMail');
+Route::post('/expense_reports/{id}/sendMail', 'ExpenseReportController@SendMail');
+
+
+Route::get('/expense_reports/{expense_report}/expenses/create', 'ExpenseController@create');
+Route::post('/expense_reports/{expense_report}/expenses', 'ExpenseController@store');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
